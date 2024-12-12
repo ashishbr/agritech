f05828ccd9194d2e8fa7214486fa0ebe24e97ce9
ashish <ashishbr12345@gmail.com>
2024-12-12 13:09:13 +0530

    database url fixed

--- agritech/settings.py
+++ agritech/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
     }
 }
 #PRODUCTION
-# database_url = os.environ.get("DATABASE_URL")
-# DATABASES["default"]= dj_database_url.parse(database_url)
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"]= dj_database_url.parse(database_url)
 
 
 #LOCAL
